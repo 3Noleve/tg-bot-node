c5f48bfdf1895a48b8e3e77a522c7d299a81a04e
3Noleve <prohoreddinson@gmail.com>
2022-10-15 14:39:07 +0300

    fix

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 
-const token = '5336424335:AAGk0uyo0qqRCrKgvr2J7GrYKK1S0MF8878';
-const webAppUrl = 'https://ornate-selkie-c27577.netlify.app';
+const token = '5750266826:AAFDXKYVEkBhi7xnCpjytZJpLkmNa3jMFAk';
+const webAppUrl = 'https://iridescent-sunburst-b7d8a9.netlify.app';
 
 const bot = new TelegramBot(token, { polling: true });
 const app = express();
